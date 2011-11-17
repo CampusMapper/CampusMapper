567916f3f583d0a3c40bf17bdf7dcc856bb65438
Alex Hart <alex.hart658@gmail.com>
2011-11-17 18:32:45 -0400

    mod js again

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-function() resizeItems() {
-    var total_height = $(document).height();
-    var header_height = $("div.ui-header").height();
-    var footer_height = $("div.ui-footer").height();
+$(document).ready(function() {
+    function() resizeItems() {
+        var total_height = $(document).height();
+        var header_height = $("div.ui-header").height();
+        var footer_height = $("div.ui-footer").height();
 
-    var height = total_height - header_height - footer_height;
-    var padding = height/6 - 82; // 64 is half the size of an icon.
+        var height = total_height - header_height - footer_height;
+        var padding = height/6 - 82; // 64 is half the size of an icon.
 
-    $("a.cm-button-link-wrapper").css("padding-top", "" + padding);
-    $("a.cm-button-link-wrapper").css("padding-bottom", "" + padding);
-};
+        $("a.cm-button-link-wrapper").css("padding-top", "" + padding);
+        $("a.cm-button-link-wrapper").css("padding-bottom", "" + padding);
+    }
+});
