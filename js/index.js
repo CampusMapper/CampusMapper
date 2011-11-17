8e1e43af50b932c6665a85143696faaa8e40f613
Alex Hart <alex.hart658@gmail.com>
2011-11-17 18:53:21 -0400

    movement

--- js/index.js
+++ js/index.js
@@ -5,11 +5,10 @@ $(document).ready(function() {
         var footer_height = $("div.ui-footer").height();
 
         var height = total_height - header_height - footer_height;
-        var padding = height/6 - 82; // 64 is half the size of an icon.
+        var padding = height/6 - 82;
 
         $("a.cm-button-link-wrapper").css("padding-top", "" + padding);
         $("a.cm-button-link-wrapper").css("padding-bottom", "" + padding);
     }
 
-    resizeItems();
 });
