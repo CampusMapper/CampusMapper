5dbf8da02509cfcbbafb188629421f2f90f6252d
Alex Hart <alex.hart658@gmail.com>
2011-11-17 17:57:35 -0400

    start on pic autoload code

---
+++ js/map.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+    // gets url parameters
+    function getUrlVars() {
+        var vars = [], hash;
+        var hashes =
+        window.location.href.slice(window.location.href.indexOf('?')
+            + 1).split('&');
+        for(var i = 0; i < hashes.length; i++)
+        {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    }
+
+    // sets a cookie
+    function setCookie(name,value,days) {
+        if (days) {
+            var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+        }
+        else var expires = "";
+        document.cookie = name+"="+value+expires+"; path=/";
+    }
+
+    // gets a cookie
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+
+    // deletes the cookie
+    function deleteCookie(name) {
+        setCookie(name,"",-1);
+    }
+
+    function init() {
+        var query = getUrlVars();
+
+        alert(query);
+        alert(document.referrer);
+    }
+});
