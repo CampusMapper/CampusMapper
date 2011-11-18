8b5e59697984468cf84244464bc72b3f504c1ee0
Alex Hart <alex.hart658@gmail.com>
2011-11-17 20:04:30 -0400

    asdf

--- js/map.js
+++ js/map.js
@@ -58,6 +58,7 @@ $(document).ready(function() {
 
         if(getUrlVars() != 'undefined') {
             query = getUrlVars();
+            quert = query['query'];
         }
 
         var src = 'img/' + query + '_map.png';
